89904195b355de90db6141ceea195626490242a5
Pana <pana.wang@outlook.com>
2025-12-09 15:49:13 +0800

    fix personal_sign tests

--- src/app_ui/sign.rs
+++ src/app_ui/sign.rs
@@ -133,7 +133,11 @@ pub fn ui_display_191_message(ctx: &Context) -> Result<bool, AppSW> {
     }];
 
     let review: NbglReview = NbglReview::new()
-        .titles("Review Message", "This is a Eip191 message", "Sign Message")
+        .titles(
+            "Review message",
+            "This is a personal message",
+            "Sign message",
+        )
         .glyph(&CFX_ICON);
 
     Ok(review.show(&my_fields))
